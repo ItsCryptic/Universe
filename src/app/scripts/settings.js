8c6fa7e377c685f5f3207dcd502ee7a7d140db1d
Kevin D <kevin.diangelis@gmail.com>
2022-09-13 18:03:08 +0000

    add warning about prompting for sudo priviliges

--- src/app/scripts/settings.js
+++ src/app/scripts/settings.js
@@ -1,15 +1,17 @@
 let exec = require("child_process").exec;
 let os = require("os");
 
-function addFlatpak() {
+function addFlathub() {
     exec("flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo");
 }
 
 function addSnap() {
     if (os.type().toString().includes("Fedora") || os.type().toString().includes("Red Hat")) {
-        exec("sudo dnf install snapd");
+        exec("sudo -i");
+        exec("dnf install snapd");
     }
-    if (os.type().toString().includes()) {
-        
+    if (os.type().toString().includes("Ubuntu") || os.type().toString().includes("Debian")) {
+        exec("sudo -i");
+        exec("apt install snapd");
     }
 }
