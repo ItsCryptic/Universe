f13ef0065377f875620df071308f8e59190103e7
Kevin D <kevin.diangelis@gmail.com>
2022-09-13 14:49:33 +0000

    add a bunch of stuff

---
+++ src/app/scripts/home.js
@@ -0,0 +1,13 @@
+var os = require("os");
+
+if (os.type().toString().includes("arch")) {
+    document.getElementById("if-arch").style.visibility = "hidden";
+}else {
+    document.getElementById("if-arch").style.visibility = "visible";
+}
+
+if (os.type().toString().includes("fedora")) {
+    document.getElementById("if-fedora").style.visibility = "hidden";
+}else {
+    document.getElementById("if-fedora").style.visibility = "visible";
+}
