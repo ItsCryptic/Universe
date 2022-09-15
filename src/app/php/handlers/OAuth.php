6c6ea0395eab2ab532fbcdaefb0b7d97d4ba2620
Kevin D <kevin.diangelis@gmail.com>
2022-09-15 02:17:57 +0000

    make some sort of progress

--- src/app/php/handlers/OAuth.php
+++ src/app/php/handlers/OAuth.php
@@ -12,4 +12,23 @@ $request = new DiscordOauth2(
 }
 
 
+$host = "";
+$username = "";
+$password = "";
+
+$conn = new mysqli($host, $username, $password);
+
+if ($conn -> connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+function login() {
+    
+}
+
+function createAccount() {
+
+}
+
 ?>
