0fe71667e9c5ff3c4a04e7db74d6306e702a48de
Kevin D <kevin.diangelis@gmail.com>
2022-09-15 18:27:17 +0000

    some work has been done on the accounts system!

--- src/app/php/handlers/OAuth.php
+++ src/app/php/handlers/OAuth.php
@@ -11,24 +11,38 @@ $request = new DiscordOauth2(
     die("Error: " . $e.getMessage());
 }
 
-
+//Database
 $host = "";
 $username = "";
 $password = "";
+$db = "";
 
-$conn = new mysqli($host, $username, $password);
+$conn = new mysqli($host, $username, $password, $db);
 
 if ($conn -> connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+$sql = "";
 echo "Connected successfully";
 
 function login() {
-    
+    if (mysqli_query($conn, $sql)) {
+        echo("Logged in.");
+    }else {
+        echo("Error logging in: " . mysqli_error($conn));
+    }
 }
 
 function createAccount() {
-
+ $sql = "INSERT INTO USERS($USERNAME, $USERPASSWORD) VALUES ($FIRST_NAME, $LAST_NAME)";
+ if ($conn->query($sql)) {
+    echo "New user created successfully";
+ }else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+ }
+ $conn.close();
+ 
 }
 
 ?>
